d35421a844074d04118dbd0266fabc46d1d51249
Malak <mlakrou280@gmail.com>
2026-02-13 19:57:09 +0100

    firebase-hosting-pr.yml uppdated added actions/setup-node@v4 and npm install

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Challenge from './components/Challenge';
-import Profile from './components/Profile';
+import Profile from './components/Profile'; 
 
 function App() {
   return (
